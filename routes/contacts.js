85e6f1c9352c9426ff929be85dd3a15b90045141
bridgea13 <bri21025@byui.edu>
2025-01-22 21:22:09 -0500

    added routes for CRUD

--- routes/contacts.js
+++ routes/contacts.js
@@ -6,4 +6,8 @@ const contactController = require('../controllers/contacts');
 router.get('/:id', contactController.getContactById);
 // Route to get all contacts
 router.get('/', contactController.getAllContacts);
+router.post('/', contactController.createContact);
+router.put('/:id', contactController.editContact);
+router.delete('/:id', contactController.deleteContact);
+
 module.exports = router;
