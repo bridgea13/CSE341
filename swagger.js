6ca40cafb9c9e399cfef8296fc2d8cd69d78a1de
bridgea13 <bri21025@byui.edu>
2025-02-02 01:40:25 -0500

    errors fixed with render swagger attempt

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title: 'My API',
         description: 'Contacts API',
     },
-    host: 'localhost: 3000',
-    schemes: ['http'],
+    host: 'https://cse341aab2025.onrender.com',
+    schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
